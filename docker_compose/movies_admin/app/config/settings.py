8d69a29d668e6b25066eea0d59c027321e77dd8a
Gwellir <gwellir@gmail.com>
2023-09-04 00:42:44 +0300

    Добавлены отдельные настройки для режима DEBUG django

--- docker_compose/movies_admin/app/config/settings.py
+++ docker_compose/movies_admin/app/config/settings.py
@@ -14,14 +14,22 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
+api_permission = os.environ.get('PERMISSION_REQUIRED', 'is_staff')
+PERMISSION_REQUIRED = api_permission if api_permission else []
+
 ALLOWED_HOSTS = tuple(os.environ.get('ALLOWED_HOSTS').split(','))
 
 
-include(
+SPLIT_SUBMODULES = [  # noqa: WPS407
     'components/application.py',
     'components/database.py',
     'components/password_validation.py',
     'components/internationalization.py',
     'components/static_files.py',
     'components/default_auto_field.py',
-)
+]
+
+if DEBUG:
+    SPLIT_SUBMODULES.append('components/debug.py')
+
+include(*SPLIT_SUBMODULES)
