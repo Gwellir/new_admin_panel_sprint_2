5a4c6b4942af06be23f702a89a985b54cdc52a55
Gwellir <gwellir@gmail.com>
2023-09-05 20:24:33 +0300

    Добавлена возможность автоматического создания суперпользователя БД

--- docker_compose/movies_admin/app/run_uwsgi.sh
+++ docker_compose/movies_admin/app/run_uwsgi.sh
@@ -5,4 +5,5 @@ chown www-data:www-data /var/log
 python manage.py compilemessages
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --no-input || true
 uwsgi --strict --ini /opt/app/uwsgi.ini
